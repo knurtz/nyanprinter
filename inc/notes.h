a73681f6d792ead82c3d2043a9c26fd77f4048c2
Johannes Herold <johannes_herold@turag.de>
2019-06-10 12:05:58 +0200

    Added defines for note frequencies

---
+++ inc/notes.h
@@ -0,0 +1,28 @@
+/*
+ * notes.h
+ *
+ *  Created on: Jun 10, 2019
+ *  Author: Johannes Herold
+ */
+
+#ifndef NOTES_H
+#define NOTES_H
+
+// only one octave needs to be defined, as one
+// octave equals half or double the frequency, a simple
+// shift left / shift right by 1 (multiply / devide by 2) does the trick.
+
+#define NOTE_C		262
+#define NOTE_CIS	277
+#define NOTE_D		294
+#define NOTE_DIS	311
+#define NOTE_E		330
+#define NOTE_F		349
+#define NOTE_FIS	370
+#define NOTE_G		392
+#define NOTE_GIS	415
+#define NOTE_A		440
+#define NOTE_AIS	466
+#define NOTE_H		494
+
+#endif /* INC_NOTES_H_ */
