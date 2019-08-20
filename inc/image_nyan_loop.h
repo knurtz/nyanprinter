f4a2a5f6dc68220db8be60a209be95f4dbf885e5
Johannes Herold <johannes_herold@turag.de>
2019-08-20 22:47:29 +0200

    [Images] Added converted nyan images

---
+++ inc/image_nyan_loop.h
@@ -0,0 +1,11 @@
+// Automatically generated header file for NyanPrinter.
+// This file contains image data from: Source/nyan_indexed_loop.png
+
+#ifndef IMAGE_NYAN_LOOP_H
+#define IMAGE_NYAN_LOOP_H
+
+#include "stm32f10x.h"
+extern const uint8_t image_nyan_loop[23808];
+extern uint16_t image_nyan_loop_length;
+
+#endif //define IMAGE_NYAN_LOOP_H
---
+++ inc/image_nyan_start.h
@@ -0,0 +1,11 @@
+// Automatically generated header file for NyanPrinter.
+// This file contains image data from: Source/nyan_indexed_start.png
+
+#ifndef IMAGE_NYAN_START_H
+#define IMAGE_NYAN_START_H
+
+#include "stm32f10x.h"
+extern const uint8_t image_nyan_start[46720];
+extern uint16_t image_nyan_start_length;
+
+#endif //define IMAGE_NYAN_START_H
