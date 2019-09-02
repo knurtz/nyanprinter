5a1fba4c53b7d50f9694f20feff6dbce3c74b902
Johannes Herold <johannes_herold@turag.de>
2019-09-02 20:56:47 +0200

    [Test Immage] New test image, diagonal line

--- inc/image_test.h
+++ inc/image_test.h
@@ -1,11 +1,11 @@
 // Automatically generated header file for NyanPrinter.
-//This file contains the image data for: test.png
+// This file contains image data from: Source/test.png
 
 #ifndef IMAGE_TEST_H
 #define IMAGE_TEST_H
 
 #include "stm32f10x.h"
-extern const uint8_t image_test[1024];
-extern uint16_t image_test_length; 	// 16 lines with 64 elements per line
+extern const uint8_t image_test[24576];
+extern uint16_t image_test_length;
 
 #endif //define IMAGE_TEST_H
